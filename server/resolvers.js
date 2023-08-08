6c1d09640d7d723e8d17923e84becefdbb639c77
sepehrmnp <sepehr.mirnasrollahi@aut.ac.ir>
2023-08-08 19:23:11 +0330

    add first server files that can handle data fetching

---
+++ server/resolvers.js
@@ -0,0 +1,26 @@
+const db = require('./db');
+
+const Query = {
+    company: (root,{id}) => db.companies.get(id),
+    job: (root,{id}) => db.jobs.get(args.id),
+    jobs: () => db.jobs.list()
+};
+
+const Company = {
+    jobs: (company) => db.jobs.list().filter((job) => job.companyId === company.id)
+};
+
+const Job = {
+    company: (job) => db.companies.get(job.companyId)
+};
+
+
+///baraye gereftan in bayad {jobs {description}} ro masalan bezani ta moshakhas beshe che field haii ro mikhay, {jobs} khaill error mide age moshakhas nakoni
+
+// oon query injory {greeting}   va   query{greeting}  farghi nadaran
+
+/// age schema ro okey koni vali to resolver taarif nakoni, mitooni call koni as console khode graphql vali faghat null migiti(//* baresi kon age in schema ro taarif nakoni chi mishe? javab ehtemali error hast)
+ 
+/// to gereftan element ha, jaye ona ro be har tartibi bezary, hamoonjoori mide
+
+module.exports = { Query, Company, Job };
